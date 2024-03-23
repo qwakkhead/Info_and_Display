43629464be2dc902e5409ec1640d147903ab96f2
qwakkhead <capusdennadrian@gmail.com>
2024-03-23 21:20:13 +0800

    Added Pseudocode

---
+++ Informations.py
@@ -0,0 +1,23 @@
+#Pseudocode first
+
+#Import fontstyle
+import fontstyle
+
+
+#State the users name
+name = input("State the users Name")
+text_name = fontstyle.apply(name, 
+					'bold/Italic/red/UNDERLINE/GREEN_BG')
+#State the users Job
+job = input("State the users Job")
+text_job = fontstyle.apply(job, 
+					'bold/Italic/red/UNDERLINE/GREEN_BG')
+#Now print it in a fancy way both Name and Job
+print (text_name)
+print (text_job)
+
+
+
+
+#Add some Informations 
+
